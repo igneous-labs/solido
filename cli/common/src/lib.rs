b52ec96ba906b0a7c49bd05ec90861d3387193a7
billythedummy <dhy1996@live.com.sg>
2022-07-27 11:35:36 +0800

    js root

--- cli/common/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use snapshot::SnapshotError;
-
-pub mod error;
-pub mod prometheus;
-pub mod snapshot;
-pub mod validator_info_utils;
-
-pub type Result<T> = std::result::Result<T, SnapshotError>;
--- cli/listener/src/main.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Chorus One AG
-// SPDX-License-Identifier: GPL-3.0
-
-fn main() {
-    listener::main()
-}
